47a52c8321cd4467222f008fa29461143318c899
Rakutenipoi <43430994+Rakutenipoi@users.noreply.github.com>
2023-07-01 19:37:04 +0800

    添加项目文件。

---
+++ Project1/utils.cpp
@@ -0,0 +1,21 @@
+#include "utils.h"
+
+void lineSegment(glm::vec3 start, glm::vec3 end, glm::vec3 color)
+{
+	std::vector<glm::vec3> vertices;
+	vertices.push_back(start);
+	vertices.push_back(end);
+
+	// 创建顶点缓冲对象
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
+
+	// 创建顶点数组对象
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+}
---
+++ Project1/utils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+void lineSegment(glm::vec3 start, glm::vec3 end, glm::vec3 color);
+
+
+
+
+
+
